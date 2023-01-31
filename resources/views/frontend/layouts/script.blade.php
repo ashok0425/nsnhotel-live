31410cb0486b300254b2e28325f1abb6f9076e73
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2023-01-31 08:52:16 +0545

    place id replace by slug in search

--- resources/views/frontend/layouts/script.blade.php
+++ resources/views/frontend/layouts/script.blade.php
@@ -1,22 +1,23 @@
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script type="text/javascript" src="{{filepath('frontend/jss/jquery.min.js')}}"></script>
-<script src="{{filepath('frontend/jss/popper.min.js')}}"></script>
-<script src="{{filepath('frontend/jss/bootstrap-4.2.1.js')}}"></script>
-<script src="{{filepath('frontend/jss/owl.carousel.min.js')}}"></script>
-<script src="{{filepath('frontend/jquery.lazy.min.js')}}"></script>
-<script src="{{filepath('frontend/jss/jqueryeasing.js')}}"></script>
-<!--<script src="{{filepath('frontend/jss/token-autocomplete.js')}}"></script>-->
-<script src="{{filepath('frontend/jss/frontcustom.js')}}"></script>
-<script src="{{filepath('frontend/jss/daterangepicker.js')}}"></script>
-<script type="text/javascript" src="{{filepath('frontend/jss/plugins.js')}}"></script>
-<script type="text/javascript" src="{{filepath('frontend/jss/scripts.js')}}"></script>
- <script type="text/javascript" src="{{filepath('frontend/jss/custom1.js')}}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key={{setting('goolge_map_api_key', 'AIzaSyBkYZ3pUwM6uOXwg9FrVfcbxXG_GmY0lrs')}}&callback=initAutocomplete&libraries=places&language={{\Illuminate\Support\Facades\App::getLocale()}}"></script>
-<script type="text/javascript" src="{{filepath('frontend/jss/map-single.js')}}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script type="text/javascript" src="{{ filepath('frontend/jss/jquery.min.js') }}"></script>
+<script src="{{ filepath('frontend/jss/popper.min.js') }}"></script>
+<script src="{{ filepath('frontend/jss/bootstrap-4.2.1.js') }}"></script>
+<script src="{{ filepath('frontend/jss/owl.carousel.min.js') }}"></script>
+<script src="{{ filepath('frontend/jquery.lazy.min.js') }}"></script>
+<script src="{{ filepath('frontend/jss/jqueryeasing.js') }}"></script>
+<!--<script src="{{ filepath('frontend/jss/token-autocomplete.js') }}"></script>-->
+<script src="{{ filepath('frontend/jss/frontcustom.js') }}"></script>
+<script src="{{ filepath('frontend/jss/daterangepicker.js') }}"></script>
+<script type="text/javascript" src="{{ filepath('frontend/jss/plugins.js') }}"></script>
+<script type="text/javascript" src="{{ filepath('frontend/jss/scripts.js') }}"></script>
+<script type="text/javascript" src="{{ filepath('frontend/jss/custom2.js') }}"></script>
+<script
+    src="https://maps.googleapis.com/maps/api/js?key={{ setting('goolge_map_api_key', 'AIzaSyBkYZ3pUwM6uOXwg9FrVfcbxXG_GmY0lrs') }}&callback=initAutocomplete&libraries=places&language={{ \Illuminate\Support\Facades\App::getLocale() }}">
+</script>
+<script type="text/javascript" src="{{ filepath('frontend/jss/map-single.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-<script src="{{filepath('frontend/build/js/intlTelInput.min.js')}}"></script>
+<script src="{{ filepath('frontend/build/js/intlTelInput.min.js') }}"></script>
 
 
 {{-- 
