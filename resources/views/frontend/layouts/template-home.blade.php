bebddcf6f1ec02aefb88d16da5f5e608f4e017ae
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2023-03-07 17:32:50 +0545

    add face book pixel

--- resources/views/frontend/layouts/template-home.blade.php
+++ resources/views/frontend/layouts/template-home.blade.php
@@ -97,6 +97,34 @@
         })(window, document, 'script', 'dataLayer', 'GTM-M4HW2BZ');
     </script>
     <!-- End Google Tag Manager -->
+
+
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '793160078411322');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=793160078411322&ev=PageView&noscript=1" /></noscript>
+    <!-- End Meta Pixel Code -->
 </head>
 
 <body>
@@ -128,19 +156,7 @@
     </script>
     <noscript><img height="1" width="1" style="display:none"
             src="https://www.facebook.com/tr?id=323172226696460&ev=PageView&noscript=1" /></noscript>
-    <!-- End Meta Pixel Code -->
-    <noscript>
-        <img height="1" width="1" src="https://www.facebook.com/tr?id=1188266251653315&ev=PageView
-&noscript=1"
-            alt="facebook" />
-    </noscript>
-    <!-- End Facebook Pixel Code -->
 
-    <noscript>
-        <img height="1" width="1" src="https://www.facebook.com/tr?id=266463828767771&ev=PageView
-&noscript=1"
-            alt="facebook" />
-    </noscript>
     <header>
         @include('frontend.layouts.topheader')
         @if (Request::segment(1) == null)
